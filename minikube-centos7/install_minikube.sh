938c45208735c4253e590d995d2bfe26f7ed36dc
Bakidjan <55193201+bakidjan@users.noreply.github.com>
2021-10-19 12:17:00 +0200

    Update install_minikube.sh

--- minikube-centos7/install_minikube.sh
+++ minikube-centos7/install_minikube.sh
@@ -21,3 +21,5 @@ echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's
 echo "Connect through ssh on the VM and then run 'minikube start --driver=none' please don't run the command as root user or using any sudo command, use vagrant user"
 sudo minikube start --driver=none
 sudo systemctl enable kubelet.service
+# The connection to the server localhost:8080 was refused - did you specify the right host or port?
+#run minikube start
